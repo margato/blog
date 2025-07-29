208e6abec4693a0e2c828ae24772c90b86a466a7
margato <margato@users.noreply.github.com>
2025-07-29 00:15:47 +0000

    deploy: 78be9e69ed8c0737d4d90525e90c438764da8970

--- en/assets/js/22f9ffc3.975181a0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[782],{6004:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- en/assets/js/acecf23e.98d411c5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/en/blog","blogTitle":"Blog","authorsListPath":"/en/blog/authors"}')}}]);
